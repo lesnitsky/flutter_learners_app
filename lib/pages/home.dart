928a99e5b278b2a2e1abce5722eae296e3163ad3
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-02-16 17:55:09 +0300

    stateful widget demo

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_learners_app/widgets/custom_list_tile.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -8,33 +9,126 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter Learners App'),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            ListTile(
-              title: Text('Title'),
-              subtitle: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam.',
-              ),
-              leading: Icon(Icons.account_box),
-              trailing: IconButton(
-                icon: Icon(Icons.more_vert),
-                onPressed: () {},
-              ),
-            ),
-            CustomListTile(
-              title: Text('Title'),
-              subtitle: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam.',
-              ),
-              leading: Icon(Icons.account_box),
-              trailing: IconButton(
-                icon: Icon(Icons.more_vert),
-                onPressed: () {},
-              ),
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Example(),
         ),
+      ),
+    );
+  }
+}
+
+class Example extends StatefulWidget {
+  @override
+  _ExampleState createState() {
+    print('create state');
+    return _ExampleState();
+  }
+}
+
+class _ExampleState extends State<Example> {
+  int count;
+  String someString;
+  final rand = Random();
+
+  @override
+  initState() {
+    count = 0;
+    someString = 'something';
+
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+  }
+
+  onAddPressed() {
+    setState(() {
+      count++;
+    });
+  }
+
+  onRemovePressed() {
+    setState(() {
+      count--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    int randomNumber = rand.nextInt(100);
+
+    return Column(
+      children: [
+        for (int i = 0; i < count; i++)
+          Box(
+            index: i,
+            randomNumber: randomNumber,
+          ),
+        RaisedButton(
+          child: Text("Add a box"),
+          onPressed: onAddPressed,
+        ),
+        RaisedButton(
+          child: Text("Remove a box"),
+          onPressed: onRemovePressed,
+        ),
+      ],
+    );
+  }
+}
+
+class Box extends StatefulWidget {
+  final int index;
+  final int randomNumber;
+
+  const Box({Key key, this.index, this.randomNumber}) : super(key: key);
+
+  @override
+  _BoxState createState() => _BoxState();
+}
+
+class _BoxState extends State<Box> {
+  @override
+  initState() {
+    print("I'm a box with index ${widget.index}");
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant Box oldWidget) {
+    print('widget with index ${widget.index} updated');
+    print('old random number: ${oldWidget.randomNumber}');
+    print('current random number: ${widget.randomNumber}');
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print("A box with index ${widget.index} was removed");
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        height: 20,
+        color: Colors.yellow,
+        child: Center(
+            child: Row(
+          children: [
+            Text(widget.index.toString()),
+            SizedBox(width: 20),
+            Text(widget.randomNumber.toString()),
+          ],
+        )),
       ),
     );
   }
